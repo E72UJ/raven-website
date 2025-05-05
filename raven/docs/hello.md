fa9189436417df82f40c473365c12b5778485b0b
Furau <3873153379@qq.com>
2025-05-06 03:47:01 +0800

    更新内容
    更新

--- raven/docs/hello.md
+++ raven/docs/hello.md
@@ -1,5 +1,7 @@
-# 你好 欢迎使用Raven 
+# 欢迎使用Raven 
 
 ## 什么是Raven？
 
 Raven是一个rust编写的视觉小说引擎，作为用户，你可以使用Raven创建专属于你自己的视觉小说系统。
+
+## 为什么要开发Raven engine
--- raven/rspress.config.ts
+++ raven/rspress.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from 'rspress/config';
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'Raven(渡鸦)-轻量双模游戏引擎',
+  head: [
+    '<meta name="raven Engine" content="视觉小说游戏引擎">',
+    // 或者
+    ['meta', { name: 'author', content: 'John Doe' }]
+
+  ],
   icon: 'https://s21.ax1x.com/2025/04/12/pERffsK.png',
   logo: {
     light: 'https://img.picui.cn/free/2025/05/03/6815c7b966355.png',
