d4ebe7a690b5e13926b489c77f845045376cb222
Furau <3873153379@qq.com>
2025-05-07 05:37:24 +0800

    sj
    sss

---
+++ raven/docs/latest.md
@@ -0,0 +1,6 @@
+# 请在这里下载 Raven engine
+:::tip
+引擎最后更新于 📅
+:::
+
+  <a href="#" class="download-btn">Download</a>
--- raven/rspress.config.ts
+++ raven/rspress.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'rspress/config';
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'Raven(渡鸦)-轻量双模游戏引擎',
+  globalStyles: path.join(__dirname, 'styles/index.css'),
   head: [
     '<meta name="raven Engine" content="视觉小说游戏引擎">',
     // 或者
